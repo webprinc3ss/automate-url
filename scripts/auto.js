eb379bba06fc4738664f3a2a8abdbc8319961f91
Mystic Kitty <djabranton@gmail.com>
2021-07-19 15:18:54 -0700

    Finished es6 refactor.

--- scripts/auto.js
+++ scripts/auto.js
@@ -13,40 +13,41 @@ const writeURL = () => {
 
 const generateURL = () => {
     //Get the URL
-    var oldLink = prompt("Enter the URL from Boise State.");
+    const oldLink = prompt("Enter the URL from Boise State.");
     console.log("oldLink", oldLink);
 
     //Get the API
-    var apiId = prompt("Enter API ID from slack");
+    const apiId = prompt("Enter API ID from slack");
     console.log("apiId", apiId);
 
     //Replace www.boisestatepublicradio with npr
-    var nprLink = oldLink;
+    let nprLink = oldLink;
     nprLink = nprLink.replace('www.boisestatepublicradio', 'npr');
     console.log('nprLink', nprLink);
 
     //Remove slug -- ie environment, new, etc
-    var noSlugLink = nprLink.replace(/npr.org\/(\w+)/, "npr.org");
+    const noSlugLink = nprLink.replace(/npr.org\/(\w+)/, "npr.org");
     console.log("noSlugLink", noSlugLink)
     //Maybe consolidate previous two steps
     //Add slashes instead of dashes in the date
     // https://regexr.com/
     //used 'negative lookahead' to avoid replacing numbers: (?!abc)	negative lookahead
-    var noDash = noSlugLink.replace(/(?!0-9\-)\-/, "/");
+    const noDash = noSlugLink.replace(/(?!0-9\-)\-/, "/");
     console.log("noDash", noDash)
 
-    var noDash2 = noDash.replace(/(?!0-9\-)\-/, "/");
+    const noDash2 = noDash.replace(/(?!0-9\-)\-/, "/");
     console.log("noDash2", noDash2);
 
     // console.log("apiId", apiId);
     //Add API!
     //With positive lookbehind: Matches a group before the main expression without including it in the result.
-    var newLink = noDash2.replace(/(?<=\d+\/\d+\/\d+\/)/, apiId + "/");
+    const newLink = noDash2.replace(/(?<=\d+\/\d+\/\d+\/)/, apiId + "/");
     console.log("newLink", newLink)
     //Write in the box!
     //https://www.boisestatepublicradio.org/environment/2021-07-15/all-hands-on-deck-national-fire-preparedness-level-reaches-highest-most-critical-tier
     //1016623768
-    return newLink; // result must be returned to pass as result of function to #url
+    // return newLink; //Removed for es6 as is 'implicit'
+    // result must be returned to pass as result of function to #url
 };
 
 generateBtn.addEventListener("click", writeURL);
